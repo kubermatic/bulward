f58748d4d5217c1fcc8a6696c84f3ad7df7097ef
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2020-06-30 19:10:24 +0800

    Rename InternalOrganization to Organization

--- pkg/apis/core/v1alpha1/organization_types.go
+++ pkg/apis/core/v1alpha1/organization_types.go
@@ -20,93 +20,93 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// InternalOrganizationSpec describes the desired state of InternalOrganization.
-type InternalOrganizationSpec struct {
-	// Metadata	contains additional human readable internalOrganization details.
-	Metadata *InternalOrganizationMetadata `json:"metadata,omitempty"`
+// OrganizationSpec describes the desired state of Organization.
+type OrganizationSpec struct {
+	// Metadata	contains additional human readable Organization details.
+	Metadata *OrganizationMetadata `json:"metadata,omitempty"`
 }
 
-// InternalOrganizationMetadata contains the metadata of the InternalOrganization.
-type InternalOrganizationMetadata struct {
-	// DisplayName is the human-readable name of this InternalOrganization.
+// OrganizationMetadata contains the metadata of the Organization.
+type OrganizationMetadata struct {
+	// DisplayName is the human-readable name of this Organization.
 	// +kubebuilder:validation:MinLength=1
 	DisplayName string `json:"displayName"`
-	// Description is the long and detailed description of the InternalOrganization.
+	// Description is the long and detailed description of the Organization.
 	// +kubebuilder:validation:MinLength=1
 	Description string `json:"description"`
 }
 
-// InternalOrganizationStatus represents the observed state of InternalOrganization.
-type InternalOrganizationStatus struct {
-	// NamespaceName is the name of the Namespace that the InternalOrganization manages.
+// OrganizationStatus represents the observed state of Organization.
+type OrganizationStatus struct {
+	// NamespaceName is the name of the Namespace that the Organization manages.
 	Namespace *ObjectReference `json:"namespace,omitempty"`
-	// ObservedGeneration is the most recent generation observed for this InternalOrganization by the controller.
+	// ObservedGeneration is the most recent generation observed for this Organization by the controller.
 	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
-	// Conditions represents the latest available observations of a InternalOrganization's current state.
-	Conditions []InternalOrganizationCondition `json:"conditions,omitempty"`
+	// Conditions represents the latest available observations of a Organization's current state.
+	Conditions []OrganizationCondition `json:"conditions,omitempty"`
 	// DEPRECATED.
 	// Phase represents the current lifecycle state of this object.
 	// Consider this field DEPRECATED, it will be removed as soon as there
 	// is a mechanism to map conditions to strings when printing the property.
 	// This is only for display purpose, for everything else use conditions.
-	Phase InternalOrganizationPhaseType `json:"phase,omitempty"`
+	Phase OrganizationPhaseType `json:"phase,omitempty"`
 }
 
-// InternalOrganizationPhaseType represents all conditions as a single string for printing by using kubectl commands.
+// OrganizationPhaseType represents all conditions as a single string for printing by using kubectl commands.
 // +kubebuilder:validation:Ready;NotReady;Unknown;Terminating
-type InternalOrganizationPhaseType string
+type OrganizationPhaseType string
 
-// Values of InternalOrganizationPhaseType.
+// Values of OrganizationPhaseType.
 const (
-	InternalOrganizationPhaseReady       InternalOrganizationPhaseType = "Ready"
-	InternalOrganizationPhaseNotReady    InternalOrganizationPhaseType = "NotReady"
-	InternalOrganizationPhaseUnknown     InternalOrganizationPhaseType = "Unknown"
-	InternalOrganizationPhaseTerminating InternalOrganizationPhaseType = "Terminating"
+	OrganizationPhaseReady       OrganizationPhaseType = "Ready"
+	OrganizationPhaseNotReady    OrganizationPhaseType = "NotReady"
+	OrganizationPhaseUnknown     OrganizationPhaseType = "Unknown"
+	OrganizationPhaseTerminating OrganizationPhaseType = "Terminating"
 )
 
 const (
-	InternalOrganizationTerminatingReason = "Deleting"
+	OrganizationTerminatingReason = "Deleting"
 )
 
 // updatePhase updates the phase property based on the current conditions.
 // this method should be called every time the conditions are updated.
-func (s *InternalOrganizationStatus) updatePhase() {
+func (s *OrganizationStatus) updatePhase() {
 	for _, condition := range s.Conditions {
-		if condition.Type != InternalOrganizationReady {
+		if condition.Type != OrganizationReady {
 			continue
 		}
 
 		switch condition.Status {
 		case ConditionTrue:
-			s.Phase = InternalOrganizationPhaseReady
+			s.Phase = OrganizationPhaseReady
 		case ConditionFalse:
-			if condition.Reason == InternalOrganizationTerminatingReason {
-				s.Phase = InternalOrganizationPhaseTerminating
+			if condition.Reason == OrganizationTerminatingReason {
+				s.Phase = OrganizationPhaseTerminating
 			} else {
-				s.Phase = InternalOrganizationPhaseNotReady
+				s.Phase = OrganizationPhaseNotReady
 			}
 		case ConditionUnknown:
-			s.Phase = InternalOrganizationPhaseUnknown
+			s.Phase = OrganizationPhaseUnknown
 		}
 		return
 	}
 
-	s.Phase = InternalOrganizationPhaseUnknown
+	s.Phase = OrganizationPhaseUnknown
 }
 
-// InternalOrganizationConditionType represents a InternalOrganizationCondition value.
+// OrganizationConditionType represents a OrganizationCondition value.
 // +kubebuilder:validation:Ready
-type InternalOrganizationConditionType string
+type OrganizationConditionType string
 
 const (
-	// InternalOrganizationReady represents a InternalOrganization condition is in ready state.
-	InternalOrganizationReady InternalOrganizationConditionType = "Ready"
+	// OrganizationReady represents a Organization condition is in ready state.
+	OrganizationReady OrganizationConditionType = "Ready"
 )
 
-// InternalOrganizationCondition contains details for the current condition of this InternalOrganization.
-type InternalOrganizationCondition struct {
-	// Type is the type of the InternalOrganization condition, currently ('Ready').
-	Type InternalOrganizationConditionType `json:"type"`
+// OrganizationCondition contains details for the current condition of this Organization.
+type OrganizationCondition struct {
+	// Type is the type of the Organization condition, currently ('Ready').
+	Type OrganizationConditionType `json:"type"`
 	// Status is the status of the condition, one of ('True', 'False', 'Unknown').
 	Status ConditionStatus `json:"status"`
 	// LastTransitionTime is the last time the condition transits from one status to another.
@@ -118,7 +118,7 @@ type InternalOrganizationCondition struct {
 }
 
 // GetCondition returns the Condition of the given condition type, if it exists.
-func (s *InternalOrganizationStatus) GetCondition(t InternalOrganizationConditionType) (condition InternalOrganizationCondition, exists bool) {
+func (s *OrganizationStatus) GetCondition(t OrganizationConditionType) (condition OrganizationCondition, exists bool) {
 	for _, cond := range s.Conditions {
 		if cond.Type == t {
 			condition = cond
@@ -130,7 +130,7 @@ func (s *InternalOrganizationStatus) GetCondition(t InternalOrganizationConditio
 }
 
 // SetCondition replaces or adds the given condition.
-func (s *InternalOrganizationStatus) SetCondition(condition InternalOrganizationCondition) {
+func (s *OrganizationStatus) SetCondition(condition OrganizationCondition) {
 	defer s.updatePhase()
 
 	if condition.LastTransitionTime.IsZero() {
@@ -156,24 +156,24 @@ func (s *InternalOrganizationStatus) SetCondition(condition InternalOrganization
 	s.Conditions = append(s.Conditions, condition)
 }
 
-// InternalOrganization is internal representation for Organization in Bulward.
+// Organization is internal representation for Organization in Bulward.
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
-// +kubebuilder:printcolumn:name="InternalOrganization Namespace",type="string",JSONPath=".status.namespace.name"
+// +kubebuilder:printcolumn:name="Organization Namespace",type="string",JSONPath=".status.namespace.name"
 // +kubebuilder:printcolumn:name="Display Name",type="string",JSONPath=".spec.metadata.displayName"
 // +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:resource:scope=Cluster,shortName=iorg
-type InternalOrganization struct {
+type Organization struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   InternalOrganizationSpec   `json:"spec,omitempty"`
-	Status InternalOrganizationStatus `json:"status,omitempty"`
+	Spec   OrganizationSpec   `json:"spec,omitempty"`
+	Status OrganizationStatus `json:"status,omitempty"`
 }
 
-// IsReady returns if the InternalOrganization is ready.
-func (s *InternalOrganization) IsReady() bool {
+// IsReady returns if the Organization is ready.
+func (s *Organization) IsReady() bool {
 	if !s.DeletionTimestamp.IsZero() {
 		return false
 	}
@@ -183,7 +183,7 @@ func (s *InternalOrganization) IsReady() bool {
 	}
 
 	for _, condition := range s.Status.Conditions {
-		if condition.Type == InternalOrganizationReady &&
+		if condition.Type == OrganizationReady &&
 			condition.Status == ConditionTrue {
 			return true
 		}
@@ -191,14 +191,14 @@ func (s *InternalOrganization) IsReady() bool {
 	return false
 }
 
-// InternalOrganizationList contains a list of InternalOrganization.
+// OrganizationList contains a list of Organization.
 // +kubebuilder:object:root=true
-type InternalOrganizationList struct {
+type OrganizationList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []InternalOrganization `json:"items"`
+	Items           []Organization `json:"items"`
 }
 
 func init() {
-	SchemeBuilder.Register(&InternalOrganization{}, &InternalOrganizationList{})
+	SchemeBuilder.Register(&Organization{}, &OrganizationList{})
 }
